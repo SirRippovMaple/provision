9d97bc417eac4f03d0f213f158990e055c23284a
Sir Rippov the Maple <git@trumpi.co.za>
2022-05-07 01:00:16 -0400

    Add fonts and other packages.

--- larbs/provision.sh
+++ larbs/provision.sh
@@ -7,12 +7,12 @@ SCRIPT_PATH=$(dirname "$SCRIPT")
 source $SCRIPT_PATH/.env
 
 while getopts ":a:r:b:p:h" o; do case "${o}" in
-	h) printf "Optional arguments for custom use:\\n  -r: Dotfiles repository (local file or url)\\n  -p: Dependencies and programs csv (local file or url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message\\n" && exit 1 ;;
-	r) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit 1 ;;
-	b) repobranch=${OPTARG} ;;
-	p) progsfile=${OPTARG} ;;
-	a) aurhelper=${OPTARG} ;;
-	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
+    h) printf "Optional arguments for custom use:\\n  -r: Dotfiles repository (local file or url)\\n  -p: Dependencies and programs csv (local file or url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message\\n" && exit 1 ;;
+    r) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit 1 ;;
+    b) repobranch=${OPTARG} ;;
+    p) progsfile=${OPTARG} ;;
+    a) aurhelper=${OPTARG} ;;
+    *) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="SirRippovMaple/dotfiles.git"
@@ -30,46 +30,47 @@ msg() { printf "%s\n" "$1" >&2; }
 error() { printf "%s\n" "$1" >&2; exit 1; }
 
 adduserandpass() {
-	# Adds user `$name` with password $pass1.
-	useradd -m -G wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
-	usermod -a -G wheel,docker "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
-	echo "$name:$pass1" | chpasswd
-	unset pass1 ;
+    # Adds user `$name` with password $pass1.
+    msg "Adding login $name"
+    useradd -m -G wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
+    usermod -a -G wheel,docker "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+    echo "$name:$pass1" | chpasswd
+    unset pass1 ;
 }
 
 refreshkeys() {
-	case "$(readlink -f /sbin/init)" in
-		*systemd* )
-			msg "Refreshing Arch Keyring..."
-			pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
-			;;
-		*)
-			msg "Enabling Arch Repositories..."
-			pacman --noconfirm --needed -S artix-keyringartix-archlinux-support >/dev/null 2>&1
-			for repo in extra community; do
-				grep -q "^\[$repo\]" /etc/pacman.conf ||
-					echo "[$repo]
+    case "$(readlink -f /sbin/init)" in
+        *systemd* )
+            msg "Refreshing Arch Keyring..."
+            pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
+            ;;
+        *)
+            msg "Enabling Arch Repositories..."
+            pacman --noconfirm --needed -S artix-keyringartix-archlinux-support >/dev/null 2>&1
+            for repo in extra community; do
+                grep -q "^\[$repo\]" /etc/pacman.conf ||
+                    echo "[$repo]
 Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
-			done
-			pacman -Sy >/dev/null 2>&1
-			pacman-key --populate archlinux >/dev/null 2>&1
-			;;
-	esac ;
+            done
+            pacman -Sy >/dev/null 2>&1
+            pacman-key --populate archlinux >/dev/null 2>&1
+            ;;
+    esac ;
 }
 
 newperms() { # Set special sudoers settings for install (or after).
-	sed -i "/#LARBS/d" /etc/sudoers
-	echo "$* #LARBS" >> /etc/sudoers ;
+    sed -i "/#LARBS/d" /etc/sudoers
+    echo "$* #LARBS" >> /etc/sudoers ;
 }
 
 installaurhelper() { # Installs $1 manually. Used only for AUR helper here.
-	# Should be run after repodir is created and var is set.
-	msg "Installing \"$1\", an AUR helper..."
-	sudo -u "$name" mkdir -p "$repodir/$1"
-	sudo -u "$name" git clone --depth 1 "https://aur.archlinux.org/$1.git" "$repodir/$1" >/dev/null 2>&1 ||
-		{ cd "$repodir/$1" || return 1 ; sudo -u "$name" git pull --force origin master;}
-	cd "$repodir/$1"
-	sudo -u "$name" -D "$repodir/$1" makepkg --noconfirm -si >/dev/null 2>&1 || return 1
+    # Should be run after repodir is created and var is set.
+    msg "Installing \"$1\", an AUR helper..."
+    sudo -u "$name" mkdir -p "$repodir/$1"
+    sudo -u "$name" git clone --depth 1 "https://aur.archlinux.org/$1.git" "$repodir/$1" >/dev/null 2>&1 ||
+        { cd "$repodir/$1" || return 1 ; sudo -u "$name" git pull --force origin master;}
+    cd "$repodir/$1"
+    sudo -u "$name" -D "$repodir/$1" makepkg --noconfirm -si >/dev/null 2>&1 || return 1
 }
 
 postInstall() {
@@ -83,68 +84,68 @@ enableServices() {
 }
 
 maininstall() { # Installs all needed programs from main repo.
-	msg "Installing \`$1\` ($n of $total)."
-	installpkg "$1"
+    msg "Installing \`$1\` ($n of $total)."
+    installpkg "$1"
     postInstall "$1"
     enableServices "$2" "$3" "$4"
 }
 
 gitmakeinstall() {
-	progname="$(basename "$1" .git)"
-	dir="$repodir/$progname"
-	msg "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1")"
-	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return 1 ; sudo -u "$name" git pull --force origin master;}
-	cd "$dir" || exit 1
-	make >/dev/null 2>&1
-	make install >/dev/null 2>&1
-	cd /tmp || return 1 ;
+    progname="$(basename "$1" .git)"
+    dir="$repodir/$progname"
+    msg "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1")"
+    sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return 1 ; sudo -u "$name" git pull --force origin master;}
+    cd "$dir" || exit 1
+    make >/dev/null 2>&1
+    make install >/dev/null 2>&1
+    cd /tmp || return 1 ;
 }
 
 aurinstall() {
-	msg "Installing \`$1\` ($n of $total) from the AUR."
-	echo "$aurinstalled" | grep -q "^$1$" && return 1
-	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1 || true
+    msg "Installing \`$1\` ($n of $total) from the AUR."
+    echo "$aurinstalled" | grep -q "^$1$" && return 1
+    sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1 || true
     postInstall "$1"
     enableServices "$2" "$3" "$4"
 }
 
 pipinstall() {
-	msg "Installing the Python package \`$1\` ($n of $total)."
-	[ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
-	yes | pip install "$1"
+    msg "Installing the Python package \`$1\` ($n of $total)."
+    [ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
+    yes | pip install "$1"
 }
 
 installationloop() {
-	([ -f "$progsfile" ] && cp "$progsfile" /tmp/pre-progs.csv) || curl -Ls "$progsfile" > /tmp/pre-progs.csv
+    ([ -f "$progsfile" ] && cp "$progsfile" /tmp/pre-progs.csv) || curl -Ls "$progsfile" > /tmp/pre-progs.csv
     cat /tmp/pre-progs.csv | sed '/^#/d' > /tmp/progs.csv
-	total=$(wc -l < /tmp/progs.csv)
+    total=$(wc -l < /tmp/progs.csv)
     msg "Installing $total packages"
-	aurinstalled=$(pacman -Qqm)
-	while IFS=, read -r tag program system_service user_service user_nosession_service; do
-		n=$((n+1))
-		case "$tag" in
-			"A") aurinstall "$program" "$system_service" "$user_service" "$user_nosession_service";;
-			"G") gitmakeinstall "$program" ;;
-			"P") pipinstall "$program" ;;
-			*) maininstall "$program" "$system_service" "$user_service" "$user_nosession_service";;
-		esac
-	done < /tmp/progs.csv ;
+    aurinstalled=$(pacman -Qqm)
+    while IFS=, read -r tag program system_service user_service user_nosession_service; do
+        n=$((n+1))
+        case "$tag" in
+            "A") aurinstall "$program" "$system_service" "$user_service" "$user_nosession_service";;
+            "G") gitmakeinstall "$program" ;;
+            "P") pipinstall "$program" ;;
+            *) maininstall "$program" "$system_service" "$user_service" "$user_nosession_service";;
+        esac
+    done < /tmp/progs.csv ;
 }
 
 putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
-	msg "Downloading and installing config files..."
-	[ -z "$3" ] && branch="master" || branch="$repobranch"
-	dir=$(mktemp -d)
-	[ ! -d "$2" ] && mkdir -p "$2"
-	chown "$name":wheel "$dir" "$2"
-	sudo -u "$name" git clone --recursive -b "$branch" --depth 1 --recurse-submodules "$1" "$dir" >/dev/null 2>&1
-	sudo -u "$name" cp -rfT "$dir" "$2"
-	}
+    msg "Downloading and installing config files..."
+    [ -z "$3" ] && branch="master" || branch="$repobranch"
+    dir=$(mktemp -d)
+    [ ! -d "$2" ] && mkdir -p "$2"
+    chown "$name":wheel "$dir" "$2"
+    sudo -u "$name" git clone --recursive -b "$branch" --depth 1 --recurse-submodules "$1" "$dir" >/dev/null 2>&1
+    sudo -u "$name" cp -rfT "$dir" "$2"
+    }
 
 systembeepoff() {
     msg "Getting rid of that error beep sound..."
-	lsmod | grep pcspkr && rmmod pcspkr
-	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;
+    lsmod | grep pcspkr && rmmod pcspkr
+    echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;
 }
 
 ### THE ACTUAL SCRIPT ###
@@ -155,21 +156,21 @@ systembeepoff() {
 refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
 for x in curl ca-certificates base-devel git ntp zsh chezmoi sudo bitwarden-cli; do
-	msg "Installing \`$x\` which is required to install and configure other programs."
-	installpkg "$x"
+    msg "Installing \`$x\` which is required to install and configure other programs."
+    installpkg "$x"
 done
 
 msg "Synchronizing time"
 ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
 
-{ id -u "$name" >/dev/null 2>&1; } && (adduserandpass || error "Error adding username and/or password")
+{ id -u "$name" >/dev/null 2>&1; } || (adduserandpass || error "Error adding username and/or password")
 mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
 
 [ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
 
 # Allow user to run sudo without password. Since AUR programs must be installed
 # in a fakeroot environment, this is required for all builds with AUR.
-newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
+newperms "%wheel ALL=(ALL:ALL) NOPASSWD: ALL"
 
 # Make pacman colorful, concurrent downloads and Pacman eye-candy.
 grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
