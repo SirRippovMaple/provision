28796b49944d7fc20cc26e6a0fa2e8106a2481ee
Sir Rippov the Maple <git@trumpi.co.za>
2022-09-12 20:57:08 -0400

    Missing script can take a parameter.

--- larbs/missing.sh
+++ larbs/missing.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env /bin/bash
+csv="${1:-progs.csv}"
 progs=$(mktemp)
 blacklist=$(mktemp)
 meta=$(mktemp)
@@ -15,6 +16,6 @@ yay
 zsh
 EOF
 pacman -Qqget | cut -f2 -d\  | sort > $meta
-cat progs.csv | grep -v '^#' | cut -f2 -d, | sort > $progs
+cat "$csv" | grep -v '^#' | cut -f2 -d, | sort > $progs
 pacman -Qqet | sort | comm -23 - $blacklist | comm -23 - $meta > /tmp/pacman.txt
 comm -23 /tmp/pacman.txt $progs
