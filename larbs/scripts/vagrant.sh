fba89747ef47a76d4e9027ab4da580c7981b1354
Sir Rippov the Maple <git@trumpi.co.za>
2023-08-26 11:21:12 -0400

    Pandoc

--- larbs/scripts/vagrant.sh
+++ larbs/scripts/vagrant.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env /bin/sh
 SCRIPT_PATH="$1"
 
-vagrant plugin install virtualbox vagrant-vbguest vagrant-share
+vagrant plugin install virtualbox vagrant-vbguest vagrant-share vagrant-disksize
