d8882b143e698f2286134ba23ef2c31470e9a727
Sir Rippov the Maple <git@trumpi.co.za>
2023-09-03 22:14:35 -0400

    Replace brave with firefox.

--- larbs/scripts/lightdm.sh
+++ larbs/scripts/lightdm.sh
@@ -6,14 +6,17 @@ replace() {
     key=$2
     value=$(printf '%s\n' "$3" | sed -e 's/[\/&]/\\&/g')
 
-    sed -i -n -e "/^$key=/!p" -e "\$a$key=$value" $file
+    sed -r -i -n -e "/^$key=/!p" -e "\$a$key=$value" $file
 }
 
 cp "$SCRIPT_PATH/etc/xprofile" /etc/xprofile
 chmod 755 /etc/xprofile
 
 mkdir -p /usr/share/backgrounds
+chmod 755 /usr/share/backgrounds
+
 cp -f "$SCRIPT_PATH/usr/share/backgrounds/plants.jpg" /usr/share/backgrounds/plants.jpg
+chmod 644 /usr/share/backgrounds/plants.jpg
 
 replace /etc/lightdm/lightdm.conf user-authority-in-system-dir true
 replace /etc/lightdm/lightdm-gtk-greeter.conf hide-user-image true
